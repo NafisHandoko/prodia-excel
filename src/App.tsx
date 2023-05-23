97cb02b25d10bff393bd7198fde07095bdd34d51
Nafis Handoko <nafis.handoko@digits.id>
2023-05-23 08:47:14 +0700

    add cellNameToIndex and customVlookup function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css';
 import {
 	CellModel,
-	RangeDirective, RangesDirective, SheetDirective, SheetsDirective,
+	RangeDirective, RangesDirective, SheetDirective, SheetModel, SheetsDirective,
 	SpreadsheetComponent,
 	getCell
 } from '@syncfusion/ej2-react-spreadsheet';
+import { Spreadsheet } from '@syncfusion/ej2-react-spreadsheet';
 import { ChangeEvent, useEffect, useState } from 'react';
 
 function App() {
 	const [sheetFile, setSheetFile] = useState<File | null>(null)
-	let hasVLookUp = []
+	const [fetchedJson, setFetchedJson] = useState(null)
+	const [hasVlookup, setHasVlookup] = useState(null)
+	const [activeSheet, setActiveSheet] = useState<SheetModel>()
 	let ssObj: SpreadsheetComponent
 
 	const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -29,6 +32,7 @@ function App() {
 		},)
 		if (resp.ok) {
 			const respJson = await resp.json()
+
 			ssObj.openFromJson({ file: respJson })
 		}
 	}
@@ -39,11 +43,107 @@ function App() {
 		}
 	}, [sheetFile])
 
+	const testCustomFunction = (str: string) => {
+		// return str + ' - Test'
+		console.log(str)
+	}
+
+	const customVlookup = (lookupValue: any, tableRange: string, colIndex: number, isShorted: any) => {
+		let spreadsheet = new Spreadsheet({})
+		let tableRange_arr: any[] = tableRange.split(':')
+		for (let i = 0; i < tableRange_arr.length; i++) {
+			tableRange_arr[i] = cellNameToIndex(tableRange_arr[i])
+		}
+		// console.log(ssObj.getActiveSheet())
+		for (let i = tableRange_arr[0][0]; i <= tableRange_arr[1][0]; i++) {
+			if (getCell(i, tableRange_arr[0][1], spreadsheet.getActiveSheet()).value == lookupValue) {
+				return getCell(i, tableRange_arr[0][1] + colIndex, spreadsheet.getActiveSheet()).value
+			}
+		}
+		// return 100
+		// let arr: string[] = []
+		// const datas = await ssObj.getData(tableRange);
+		// return datas.forEach((data, i) => {
+		// 	// arr.push(data.value as string)
+		// 	if (data.value == lookupValue) {
+		// 		return data.value;
+		// 	}
+		// });
+		// console.log(datas.keys())
+		// console.log(arr)
+
+		// return new Promise(async (resolve) => {
+		// 	const datas = await ssObj.getData(tableRange);
+		// 	datas.forEach((data, i) => {
+		// 		if (data.value == lookupValue) {
+		// 			resolve(data.value);
+		// 		}
+		// 	});
+		// 	resolve(undefined); // Value not found
+		// });
+
+		// range.forEach((data) => {
+		// 	arr.push(data.value as string)
+		// })
+		// if(arr[0]=='Manager'){
+		// 	console.log('berhasil')
+		// }
+		// console.log(lookupValue)
+		// for (let i = 0; i < arr.length; i++) {
+		// 	if (arr[i] == lookupValue) {
+		// 		console.log('berhasil')
+		// 		// console.log(arr[i + 1])
+		// 		// return arr[i + (colIndex - 1)];
+		// 	}
+		// }
+	}
+
+	useEffect(() => {
+		setActiveSheet(ssObj.getActiveSheet())
+		ssObj.addCustomFunction(testCustomFunction, 'TEST')
+		ssObj.addCustomFunction(customVlookup, 'VLOOKUP')
+	}, [])
+
 	const testAction = async () => {
 		// const x = await ssObj.getData('A1')
 		// console.log(x)
 		// ssObj.goTo('B5')
 		// let cell: CellModel = getCell(0, 0, ssObj.getActiveSheet()); // rowIndex, colIndex, sheetIndex
+		// const sheet = spreadsheet.getActiveSheet();
+
+		// let cell: CellModel = getCell(3, 3, ssObj.getActiveSheet()); // rowIndex, colIndex, sheetIndex
+		// const formula = cell.formula
+		// vlookupTest(formula as string)
+		console.log(ssObj.getIndexes('A1'))
+	}
+
+	const vlookupTest = async (formula: string) => {
+		// const computed = ssObj.computeExpression('=G5+G6')
+		// console.log(computed)
+		// const selectedCell = await ssObj.getData('D4')
+		// let cell: CellModel = getCell(3, 3, ssObj.getActiveSheet()); // rowIndex, colIndex, sheetIndex
+		// const formula = cell.formula
+		// const computed = ssObj.computeExpression(formula as string)
+		// console.log(computed)
+		const lookupValue = 'Manager'
+		let arr: string[] = []
+		const range = await ssObj.getData('F4:G6')
+		range.forEach((data) => {
+			arr.push(data.value as string)
+		})
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] === lookupValue) {
+				// return arr[i + 1];
+				console.log(arr[i + 1])
+			}
+		}
+		// for (let i = 0; i < values.length; i++) {
+		// 	if (values[i][0] === lookupValue) {
+		// 		return values[i][colIndex - 1];
+		// 	}
+		// }
+
+		return "#N/A"; // Value not found
 	}
 
 	const indexToCellName = (row: number, col: number) => {
@@ -64,19 +164,18 @@ function App() {
 
 	}
 
-	function vlookup(lookupValue: any, table: any, colIndex: any, exactMatch: any) {
-		for (let i = 0; i < table.length; i++) {
-			if (exactMatch) {
-				if (table[i][0] === lookupValue) {
-					return table[i][colIndex - 1];
-				}
-			} else {
-				if (table[i][0] == lookupValue) {
-					return table[i][colIndex - 1];
-				}
+	const cellNameToIndex = (cellName: string) => {
+		if (cellName) {
+			const column = cellName.match(/[A-Z]+/)![0];
+			const row = parseInt(cellName.match(/[0-9]+/)![0]) - 1;
+
+			let columnIndex = 0;
+			for (let i = 0; i < column.length; i++) {
+				columnIndex += (column.charCodeAt(i) - 65 + 1) * Math.pow(26, column.length - i - 1);
 			}
+
+			return [row, columnIndex - 1];
 		}
-		return "#N/A"; // Value not found
 	}
 
 	return (
